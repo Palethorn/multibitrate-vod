cee687bf323c8ea104689b37627f15b460c2939c
David Cavar <wizzard405@gmail.com>
2016-12-20 21:06:20 +0100

    Use jinja2 for command template render

--- transcode.py
+++ transcode.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-
 import yaml
 import sys
 import os
+from jinja2 import Template
 
-pass1 = 'ffmpeg -y -i {0} -vf drawtext=\"fontfile=/usr/share/fonts/ubuntu/Ubuntu-B.ttf: text=\'{4}\': fontcolor=white: fontsize=48: box=1: boxcolor=black: x=0: y=0\" -c:a libfdk_aac -ac 2 -ab {1} -c:v libx264 -preset:v veryfast -threads 0 -r 25 -g 48 -keyint_min 48 -sc_threshold 0 -x264opts bframes=1 -pass 1 -b:v {2} -profile:v {3} -s {4} -f mp4 {5}'
-pass2 = 'ffmpeg -y -i {0} -vf drawtext=\"fontfile=/usr/share/fonts/ubuntu/Ubuntu-B.ttf: text=\'{4}\': fontcolor=white: fontsize=48: box=1: boxcolor=black: x=0: y=0\" -c:a libfdk_aac -ac 2 -ab {1} -c:v libx264 -preset:v fast -threads 0 -r 25 -g 48 -keyint_min 48 -sc_threshold 0 -x264opts bframes=1 -pass 2 -b:v {2} -profile:v {3} -s {4} -f mp4 {5}'
+drawtext = '-vf drawtext=\"fontfile=/usr/share/fonts/ubuntu/Ubuntu-B.ttf: text=\'{{ text }}\': fontcolor=white: fontsize=48: box=1: boxcolor=black: x=0: y=0\"'
+
+pass1 = Template('ffmpeg -y -i {{ input_file }} -t 00:5:00 -c:a aac -ac 2 -ab {{ audio_bitrate }} -c:v libx264 -preset:v veryfast -threads 0 -r {{ fps }} -g {{ gop }} -keyint_min {{ gop }} -sc_threshold 0 -x264opts bframes=1 -pass 1 -b:v {{ bitrate }} -profile:v {{ profile }} -s {{ size }} -f mp4 {{ output_file }}')
+pass2 = Template('ffmpeg -y -i {{ input_file }} -t 00:5:00 -c:a aac -ac 2 -ab {{ audio_bitrate }} -c:v libx264 -preset:v fast -threads 0 -r {{ fps }} -g {{ gop }} -keyint_min {{ gop }} -sc_threshold 0 -x264opts bframes=1 -pass 2 -b:v {{ bitrate }} -profile:v {{ profile }} -s {{ size }} -f mp4 {{ output_file }}')
 
 for file in sys.argv:
 
@@ -14,8 +15,11 @@ for file in sys.argv:
 
     stream = open(file + '.yml', 'r')
     qualities = yaml.load(stream)['qualities']
+
     for q in qualities:
-        command = pass1.format(file, qualities[q]['audio_bitrate'], qualities[q]['bitrate'], qualities[q]['profile'], qualities[q]['size'], file.replace('.mp4', '') + '_' + q + '.mp4')
+        qualities[q]['input_file'] = file
+        qualities[q]['output_file'] = file.replace('.mp4', '') + '_' + q + '.mp4'
+        command = pass1.render(qualities[q])
         os.system(command)
-        command = pass2.format(file, qualities[q]['audio_bitrate'], qualities[q]['bitrate'], qualities[q]['profile'], qualities[q]['size'], file.replace('.mp4', '') + '_' + q + '.mp4')
+        command = pass2.render(qualities[q])
         os.system(command)
